7ed80f6cd967bd613f6954ab45c0c70c7d051ac6
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2023-09-04 20:56:18 -0300

    deploy app

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -5,5 +5,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse){
     const { id } = req.query
 
     const product = products.find(product => product.id === Number(id))
+
+    if (!product) {
+        res.status(404).json({ error: 'Produto não encontrado' })
+        return
+    }
+
     res.status(200).json(product)
 }
